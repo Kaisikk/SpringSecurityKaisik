cb0173788f72655d9bb02aa76c7e17b14d821f1a
Kaisikk <Kosikov.Ilya@mail.ru>
2024-03-03 23:22:49 +0400

    Change Config

--- src/main/java/com/kaisikk/java/springsecurity/springsecurity/controllers/AppController.java
+++ src/main/java/com/kaisikk/java/springsecurity/springsecurity/controllers/AppController.java
@@ -3,6 +3,7 @@ package com.kaisikk.java.springsecurity.springsecurity.controllers;
 import com.kaisikk.java.springsecurity.springsecurity.model.Application;
 import com.kaisikk.java.springsecurity.springsecurity.services.AppService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,11 +24,13 @@ public class AppController {
     }
 
     @GetMapping("/all-app")
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     public List<Application> allApplications() {
         return service.allApplications();
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
         public Application applicationById(@PathVariable int id){
         return service.applicationById(id);
         }
